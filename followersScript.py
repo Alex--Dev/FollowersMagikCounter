bfe6b847daed306133cdc0dc1833cc20376ef89e
Bercero <gordilocuente@gmail.com>
2020-03-26 18:57:26 +0100

    ahora se puede usar en linux, pequeñas correciones

--- followersScript.py
+++ followersScript.py
@@ -1,33 +1,42 @@
-import msvcrt
+#! /usr/bin/env python3.6
+from platform import system
+if system == 'Windows':
+    from msvcrt import getch
+else:
+    from getch import getch #require haber instalado getch mediante pip
 
 def leerDatos():
-    print('\n')
+    print('\nUsuarios:\n')
     with open("datos", 'r') as f:
         dato = f.readline()
         while dato != '':
             print(dato, end='')
             dato = f.readline()
+    print()
 
 def escribirDato():
-    print('Escriba el usuario a añadir: ')
+    print('\nEscriba el usuario a añadir: ')
     newUser = input()
     with open("datos", 'a') as f:
         f.write (newUser)
         f.write('\n')
+    print('Añadido\n')
 
-def buscarUsuario(user):
-        with open("datos", 'r') as f:
-            encontrado = False
-            dato = f.readline()
-            while dato != '':
-                if dato == user
-                    print("El usuario ya se encuentra en la lista")
-                    encontrado = True
-                else
-                    dato = f.readline()
-            if !encontrado:
-                print('El usuario no se encuentra en la lista')
-
+def buscarUsuario():
+    print('\nBuscar usuario:')
+    user = input()
+    with open("datos", 'r') as f:
+        encontrado = False
+        dato = f.readline().split()[0]
+        while dato != '':
+            if dato == user:
+                print("El usuario ya se encuentra en la lista\n")
+                encontrado = True
+                break
+            else:
+                dato = f.readline().split()[0]
+        if not encontrado:
+            print('El usuario NO se encuentra en la lista\n')
 
 if __name__ == '__main__':
     print('FOLLOWERS TWITTER\n')
@@ -38,11 +47,13 @@ if __name__ == '__main__':
         print('Pulse 2 para añadir un nuevo user a la lista')
         print('Pulse 3 para buscar un usuario en la lista')
         print('Para salir pulse 0')
-        opcion = int (msvcrt.getche())
+        opcion = int (getch())
         switcher  = {
-            0: break#salir
-            1: leerDatos()
-            2: escribirDato()
-            3: buscarUsuarios()
+            0: lambda: 0, #salir
+            1: leerDatos,
+            2: escribirDato,
+            3: buscarUsuario
         }
+        switcher[opcion]()
+
     print('¡Hasta Luego!')
