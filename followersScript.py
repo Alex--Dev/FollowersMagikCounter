4edd1bb3faf70aa30b17fd5d65794f1252411547
Alex--Dev <androalex@hotmail.com>
2020-03-26 12:44:25 +0100

    Primera subida: Definiendo la estructura. TODO: No funciona la opcion SALIR del menu

---
+++ followersScript.py
@@ -0,0 +1,29 @@
+import msvcrt
+
+def leerDatos():
+    with open("datos", 'a') as f:
+        dato = f.readline()
+        while dato != '':
+            print(dato, end='')
+            dato = f.readline()
+
+
+def escribirDato():
+    with open("datos", 'a') as f:
+        f.write ('@NombrePrueba\n')
+
+if __name__ == '__main__':
+    print('FOLLOWERS TWITTER\n')
+    opcion = '4'
+    #Display MENU
+    while opcion != '0':
+        print('pulse 1 para buscar un user en la lista')
+        print('Pulse 2 para añadir un nuevo user a la lista')
+        print('Para salir pulse 0')
+        opcion = msvcrt.getche()
+        if opcion == '1':
+            leerDatos() #De momento MUESTRA POR PANTALLA
+        elif opcion == '2':
+            escribirDato() #TODO: implementar paso parametro
+
+    print('¡Hasta Luego!')
