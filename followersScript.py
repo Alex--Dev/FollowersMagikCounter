eb9b240d64a43ac924aca63bcc8085c7244887ab
Alex--Dev <androalex@hotmail.com>
2020-03-26 13:13:54 +0100

    Solucionado el input del MENU, era necesario parsear a INT.

--- followersScript.py
+++ followersScript.py
@@ -14,16 +14,16 @@ def escribirDato():
 
 if __name__ == '__main__':
     print('FOLLOWERS TWITTER\n')
-    opcion = '4'
+    opcion = 4
     #Display MENU
-    while opcion != '0':
+    while opcion != 0:
         print('pulse 1 para buscar un user en la lista')
         print('Pulse 2 para añadir un nuevo user a la lista')
         print('Para salir pulse 0')
-        opcion = msvcrt.getche()
-        if opcion == '1':
+        opcion = int (msvcrt.getche())
+        if opcion == 1:
             leerDatos() #De momento MUESTRA POR PANTALLA
-        elif opcion == '2':
+        elif opcion == 2:
             escribirDato() #TODO: implementar paso parametro
-
+        print('\n')
     print('¡Hasta Luego!')
